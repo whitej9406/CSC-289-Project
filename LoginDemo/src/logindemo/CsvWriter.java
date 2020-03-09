5e12bb654b1d5267574958efaf260ed8870be044
mcdonald5764 <43609350+mcdonald5764@users.noreply.github.com>
2020-03-09 02:24:01 -0400

    Add files via upload

--- LoginDemo/src/logindemo/CsvWriter.java
+++ LoginDemo/src/logindemo/CsvWriter.java
@@ -6,10 +6,35 @@ import java.io.PrintWriter;
 import javax.swing.JOptionPane;
 
 public class CsvWriter {
-    public static void writeRecord(){      
+    public static void writeRecord(){ 
+       boolean checkingPass = true;
        String fileName = "accounts.csv";
+       
+       String first  = JOptionPane.showInputDialog("Enter First Name: ");
+       String last  = JOptionPane.showInputDialog("Enter Last Name: ");
+       
+       // TO DO 
+       // Needs to check the list of usernames and make sure it is not already
+       // inside of the list
+       
        String user  = JOptionPane.showInputDialog("Enter Username: ");
-       String pass  = JOptionPane.showInputDialog("Enter Password: ");
+       
+       
+       String pass = "";
+       while (checkingPass == true){
+           pass  = JOptionPane.showInputDialog("Enter Password: ");
+       
+           String passCheck  = JOptionPane.showInputDialog("Enter Password again: ");
+           
+           if (pass.equals(passCheck)){
+               checkingPass = false;
+               
+           }else{
+              JOptionPane.showMessageDialog(null,"Passwords don't match!"); 
+           }
+       }
+       
+
       try
         {
             FileWriter writer = new FileWriter(fileName,true);
